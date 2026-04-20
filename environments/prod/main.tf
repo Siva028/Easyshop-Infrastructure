36e454449a921ef16b4036d0126803c2e2466769
Siva <siva.subramanian3@icloud.com>
2026-04-20 12:38:07 +0000

    Fix formatting in GitHub Actions IAM role policy for consistency

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -88,8 +88,7 @@ resource "aws_iam_role" "github_actions" {
       Action = "sts:AssumeRoleWithWebIdentity"
       Condition = {
         StringLike = {
-          "token.actions.githubusercontent.com:sub" =
-            "repo:${var.github_org}/${var.github_repo}:*"
+          "token.actions.githubusercontent.com:sub" = "repo:${var.github_org}/${var.github_repo}:*"
         }
         StringEquals = {
           "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
